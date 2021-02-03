e1bdf251fef9b91eac18743292964a2ce1c5556c
scidatmath2020 <56654363+scidatmath2020@users.noreply.github.com>
2021-02-03 15:57:27 -0600

    Add files via upload

--- script/practica01.R
+++ script/practica01.R
@@ -1,12 +1,14 @@
 setwd("C:\\Users\\hp master\\OneDrive\\Escritorio\\taller_icd_rm")
 getwd()
 
+install.packages("DMwR")
+library(DMwR)
+mfv(c(1,1,1,3,3,1,2,4,4,4,4))
+
 #Con esto hacemos comentarios 
 dir()  #dir() sirve para enlistar los archivos que tengo en la ubicación
 
 maraton <- read.csv("Maraton_NY.csv", header = TRUE, row.names = 1)
-
-View(maraton)
 
 head(maraton,3)
 dim(maraton)
@@ -67,6 +69,111 @@ write.csv(latam,"tabla_latam.csv",row.names=FALSE)
  
 latam[latam$age>20,]
 
+########
 
+View(maraton)
+
+maraton$categoria <- cut(maraton$time,breaks=3,labels=c("alto","medio","bajo"))
+
+max(maraton$time)
+
+intervalos <- c(-Inf,100,200,300,Inf)
+maraton$nva_categoria <- cut(maraton$time,breaks=intervalos,labels=c("c1","c2"))
+
+summary(maraton$nva_categoria)
+
+
+dim(latam)
+
+min(latam$time)
+
+which.min(latam$time)
+latam[37,]
+
+
+latam[which.min(latam$time),]$age
+
+ejemplo <- data.frame(V1=c("a","b","c","d","e"),V2=c(1,1,6,2,6))
+
+which.max(ejemplo$V2)
+ejemplo
+
+ejemplo[ejemplo$V2 == min(ejemplo$V2),]
+
+contaminacion <- read.csv("contaminacion.csv", header = TRUE)
+
+install.packages("mice")
+library(mice)
+
+md.pattern(contaminacion)
+
+conta0 <- contaminacion[-33,]
+
+md.pattern(conta0)
+
+View(conta0)
+
+
+is.na(conta0$CN)
+conta0$Numero_de_empresas[is.na(conta0$CN)]
+
+conta0[is.na(conta0$CN),]
+
+conta0
+complete.cases(conta0)
+
+conta0[complete.cases(conta0),]  #na.omit(conta0)
+
+md.pattern(na.omit(conta0))
+
+conta0
+
+sum(conta0$CN)
+
+### NA+numero = NA; NA*numero=NA; NA/número = NA; número/NA
+
+sum(conta0$CN,na.rm=TRUE)
+mean(conta0$CN,na.rm=TRUE)
+
+
+conta0$CN_medias <- ifelse(is.na(conta0$CN),mean(conta0$CN,na.rm=TRUE),conta0$CN)
+
+B <- data.frame(V1 = c("a","b","c","d","e"),V2 = c("practica","hola","hola",NA,"adios"))
+B
+
+install.packages("modeest")
+library(modeest)
+
+B$V2_modas <- ifelse(is.na(B$V2),mfv(B$V2,na_rm=TRUE),B$V2)
+B
+
+summary(factor(B$V2))
+
+summary(factor(c("a","b","c")))
+
+
+####################
+
+install.packages("DMwR")
+library(DMwR)
+
+conta0
+conta1 <- knnImputation(conta0)
+
+View(conta1)
+######################
+
+duplicated(conta0)
+View(conta0)
+which(duplicated(conta0))
+
+
+conta_sin_dupl <- unique(conta0)
+
+dim(conta_sin_dupl)
+
+duplicated(conta_sin_dupl)
+
+conta0[26,]
 
 
